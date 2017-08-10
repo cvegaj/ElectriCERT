bd5ccb5184e86bf7a3c811ac6f23df87a01d56dc
cvegaj <carlavj93@gmail.com>
2017-08-10 00:54:21 -0600

    Tools and scripts updated
    Fixed issues with cert tools and scripts

--- script-issuer.sh
+++ script-issuer.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "Moving old blockchain_certificates to backup folder..."
+mv ./cert-issuer/data/blockchain_certificates/* ./cert-issuer/data/backup/blockchain_certificates/
+echo "Activating virtual enviroment..."
 source venv3/bin/activate
 cd cert-issuer
 python3 issue_certificates.py -c confx.ini
-cp -r /Users/carlavega/Documents/Proyecto/cert-issuer/data/blockchain_certificates/* /Users/carlavega/Documents/Proyecto/cert-viewer/cert_data/
+echo "Moving blockchain_certificates to cert-data folder..."
+cp -r ../cert-issuer/data/blockchain_certificates/* ../cert-viewer/cert_data/
+echo "Moving processed unsigned_certificates to backup folder..."
+mv ../cert-issuer/data/unsigned_certificates/* ../cert-issuer/data/backup/unsigned_certificates/
 deactivate
--- script-tools.sh
+++ script-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd cert-tools
-cp -r /Users/carlavega/Documents/Proyecto/cert-tools/sample_data/unsigned_certificates/* /Users/carlavega/Documents/Proyecto/cert-tools/sample_data/backup/
+mv ../cert-tools/sample_data/unsigned_certificates/* ../cert-tools/sample_data/backup/
 create-certificate-template -c confx.ini
 instantiate-certificate-batch -c confx.ini
-cp -r /Users/carlavega/Documents/Proyecto/cert-tools/sample_data/unsigned_certificates/* /Users/carlavega/Documents/Proyecto/cert-issuer/data/unsigned_certificates/
+cp -r ../cert-tools/sample_data/unsigned_certificates/* ../cert-issuer/data/unsigned_certificates/
